467f127fd987fad91d23d5be7046f157caec7241
Ty <vxlk1@yahoo.com>
2018-11-02 10:03:00 -0400

    Add files via upload

---
+++ Scripts/ArrowScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour {
+
+    public int damage;
+    //public ScoreKeeper skeep;
+    public string playerTag;
+
+    void Start()
+    {
+       // skeep = GameObject.Find("_GM").GetComponent<ScoreKeeper>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == playerTag)
+        {
+           // skeep.selectedGO = col.gameObject;
+            //skeep.selected = true;
+            PlayerStats stats = col.gameObject.GetComponent<PlayerStats>();
+            stats.Hit(damage);
+        }
+    }
+}
